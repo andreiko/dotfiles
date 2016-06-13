3567392dbeecf91664887a2983aafd6f5b83100b
Andrey Bulgakov <mail@andreiko.ru>
2016-06-13 13:39:22 +0400

    Update .workspaces.sh

--- .workspaces.sh
+++ .workspaces.sh
@@ -12,3 +12,21 @@ function goon () {
         cd $STARTDIR
     fi
 }
+
+function _goon_gopaths {
+    local cur prev opts n
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    COMPREPLY=()
+    opts=$(ls "${HOME}/go" 2> /dev/null)
+
+    if (( $? != 0 )); then
+        return
+    fi
+
+    if (( $COMP_CWORD == 1 )); then
+        COMPREPLY=( $(compgen -W "${opts[@]}" -- $cur) )
+    fi
+}
+
+complete -F _goon_gopaths goon
